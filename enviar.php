e5f7911781e6869ec9d24ca38106eb227b15352c
Luis Rayo <rayo862@hotmail.com>
2021-12-29 14:18:53 -0500

    01

--- enviar.php
+++ enviar.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 
 // Declaración de variables del formulario
 $nombre = $_POST['name'];
@@ -25,26 +25,4 @@ if ($_POST['submit']) {
     }
 }
 
-?> -->
-<?php
-$nombre = $_POST['name'];
-$mail = $_POST['email'];
-$empresa = $_POST['message'];
-
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
-
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
-
-$para = 'rayo862@hotmail.com';
-$asunto = 'Mensaje de mi sitio web';
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-header("Location:index.html");
-?>
+?> 
